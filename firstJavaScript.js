708da32c4ce21e8d58396df9c98f611632339934
sahil-student <sahilguleria@gmail.com>
2023-12-25 21:17:03 +0530

    new update

---
+++ firstJavaScript.js
@@ -0,0 +1,12 @@
+console.log("Wellcome to javascript Sahil Guleria");
+let a =("sahil");
+console.log(a);
+const student ={
+    fullName : "Sahil guleria",
+    number : 88,
+    age : 23,
+}
+console.log(student);
+function myFunction(){
+    console.log(student);
+}
